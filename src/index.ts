c06d10d5adcb534c2733ec00ecd1c48c51097e37
Igor Souza <igor.souza.webmaster@gmail.com>
2020-04-04 23:18:16 -0300

    feat: add functionalities inside functions

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,8 @@ export class Main {
     constructor() {
       const canvas = document.getElementById("canvas") as HTMLCanvasElement;
       const engine = new Engine(canvas, true);
+      const cameraDistance = 25;
+      let tempv = new Vector3(0, 0, 0);
       
       const createScene = function() {
         const scene = new Scene(engine);
@@ -39,112 +41,134 @@ export class Main {
 
         return inputMap;
       }
-
       const scene = createScene();
       const inputMap = sceneInput();
 
-      // Lights
-      var torch = new PointLight("light1", Vector3.Zero(), scene);
-      torch.intensity = 0.7;
-      torch.diffuse = Color3.FromHexString('#ff9944');
+      const createPlayer = function() {
+        // Material
+        const player1Mat = new StandardMaterial("pmat", scene);
+        player1Mat.emissiveColor = Color3.FromHexString('#ff9900');
+        player1Mat.specularPower = 64;
 
-      var sky = new HemisphericLight("sky", new Vector3(0, 1.0, 0), scene);
-      sky.intensity = 0.5;
+        const playereMat = new StandardMaterial("pemat", scene);
+        playereMat.emissiveColor = Color3.FromHexString('#ffffff');
+        playereMat.specularPower = 128;
 
-      // Material
-      var player1Mat = new StandardMaterial("pmat", scene);
-      player1Mat.emissiveColor = Color3.FromHexString('#ff9900');
-      player1Mat.specularPower = 64;
+        const playerbMat = new StandardMaterial("pbmat", scene);
+        playerbMat.diffuseColor = Color3.Black();
 
-      var playereMat = new StandardMaterial("pemat", scene);
-      playereMat.emissiveColor = Color3.FromHexString('#ffffff');
-      playereMat.specularPower = 128;
+        // Player ----
+        const player = Mesh.CreateSphere("playerbody", 8, 1.8, scene);
+        player.material = player1Mat;
+        player.position.y = 0.9;
 
-      var playerbMat = new StandardMaterial("pbmat", scene);
-      playerbMat.diffuseColor = Color3.Black();
+        const playere1 = Mesh.CreateSphere("eye1", 8, 0.5, scene);
+        playere1.material = playereMat;
+        playere1.position.y = 0.5;
+        playere1.position.z = 0.5;
+        playere1.position.x = -0.3;
+        playere1.parent = player;
 
-      // Player ----
-      const player = Mesh.CreateSphere("playerbody", 8, 1.8, scene);
-      player.material = player1Mat;
-      player.position.y = 0.9;
+        const playere2 = Mesh.CreateSphere("eye2", 8, 0.5, scene);
+        playere2.material = playereMat;
+        playere2.position.y = 0.5;
+        playere2.position.z = 0.5;
+        playere2.position.x = 0.3;
+        playere2.parent = player;
 
-      var playere1 = Mesh.CreateSphere("eye1", 8, 0.5, scene);
-      playere1.material = playereMat;
-      playere1.position.y = 0.5;
-      playere1.position.z = 0.5;
-      playere1.position.x = -0.3;
-      playere1.parent = player;
+        const playereb1 = Mesh.CreateSphere("eye1b", 8, 0.25, scene);
+        playereb1.material = playerbMat;
+        playereb1.position.y = 0.5;
+        playereb1.position.z = 0.7;
+        playereb1.position.x = -0.3;
+        playereb1.parent = player;
 
-      var playere2 = Mesh.CreateSphere("eye2", 8, 0.5, scene);
-      playere2.material = playereMat;
-      playere2.position.y = 0.5;
-      playere2.position.z = 0.5;
-      playere2.position.x = 0.3;
-      playere2.parent = player;
+        const playereb2 = Mesh.CreateSphere("eye2b", 8, 0.25, scene);
+        playereb2.material = playerbMat;
+        playereb2.position.y = 0.5;
+        playereb2.position.z = 0.7;
+        playereb2.position.x = 0.3;
+        playereb2.parent = player;
 
-      var playereb1 = Mesh.CreateSphere("eye1b", 8, 0.25, scene);
-      playereb1.material = playerbMat;
-      playereb1.position.y = 0.5;
-      playereb1.position.z = 0.7;
-      playereb1.position.x = -0.3;
-      playereb1.parent = player;
+        player.checkCollisions = true;
+        player.ellipsoid = new Vector3(0.9, 0.45, 0.9);
 
-      var playereb2 = Mesh.CreateSphere("eye2b", 8, 0.25, scene);
-      playereb2.material = playerbMat;
-      playereb2.position.y = 0.5;
-      playereb2.position.z = 0.7;
-      playereb2.position.x = 0.3;
-      playereb2.parent = player;
+        return player;
+      }
+      const player = createPlayer();
 
-      player.checkCollisions = true;
-      player.ellipsoid = new Vector3(0.9, 0.45, 0.9);
+      const sceneLight = function() {
+        const torch = new PointLight("light1", Vector3.Zero(), scene);
+        torch.intensity = 0.7;
+        torch.diffuse = Color3.FromHexString('#ff9944');
+        
+        return torch;
+      }
+
+      const sceneLightImpostor = function() {
+        var lightImpostor = Mesh.CreateSphere("sphere1", 16, 0.1, scene);
+        var lightImpostorMat = new StandardMaterial("mat", scene);
+        lightImpostor.material = lightImpostorMat;
+        lightImpostorMat.emissiveColor = Color3.Yellow();
+        lightImpostorMat.linkEmissiveWithDiffuse = true;
+        lightImpostor.position.y = 0;
+        lightImpostor.position.z = 0;
+        lightImpostor.position.x = 0;
+        lightImpostor.parent = player;
+
+        return lightImpostor;
+      }
+
+      const sceneSky = function() {
+        const sky = new HemisphericLight("sky", new Vector3(0, 1.0, 0), scene);
+        sky.intensity = 0.5;
+
+        return sky;
+      }
+      const torch = sceneLight();
+      const sky = sceneSky();
+      const lightImpostor = sceneLightImpostor();
+      
       let playerSpeed = new Vector3(0, 0, 0.08);
       let playerNextspeed = new Vector3(0, 0, 0);
       let playerNexttorch = new Vector3(0, 0, 0);
 
-      // Camera
-      const cameraDistance = 25;
-      const camera = new FollowCamera("FollowCam", new Vector3(player.position.x, player.position.y + 5, player.position.z - 45), scene);
-      camera.radius = cameraDistance; // how far from the object to follow
-      camera.heightOffset = 3; // how high above the object to place the camera
-      camera.rotationOffset = 90; // the viewing angle
-      camera.cameraAcceleration = 0; // how fast to move
-      camera.maxCameraSpeed = 20; // speed limit
-      camera.attachControl(canvas, true);
-      camera.lockedTarget = player; // target any mesh or object with a "position" Vector3
-      
-      var lightImpostor = Mesh.CreateSphere("sphere1", 16, 0.1, scene);
-      var lightImpostorMat = new StandardMaterial("mat", scene);
-      lightImpostor.material = lightImpostorMat;
-      lightImpostorMat.emissiveColor = Color3.Yellow();
-      lightImpostorMat.linkEmissiveWithDiffuse = true;
-      lightImpostor.position.y = 0;
-      lightImpostor.position.z = 0;
-      lightImpostor.position.x = 0;
-      lightImpostor.parent = player;
+      const createCamera = function() {
+        const camera = new FollowCamera("FollowCam", new Vector3(player.position.x, player.position.y + 5, player.position.z - 45), scene);
+        camera.radius = cameraDistance; // how far from the object to follow
+        camera.heightOffset = 3; // how high above the object to place the camera
+        camera.rotationOffset = 90; // the viewing angle
+        camera.cameraAcceleration = 0; // how fast to move
+        camera.maxCameraSpeed = 20; // speed limit
+        camera.attachControl(canvas, true);
+        camera.lockedTarget = player; // target any mesh or object with a "position" Vector3
 
-      // Ground
-      var ground = Mesh.CreatePlane("g", 120, scene);
-      ground.position = new Vector3(0, 0, 0);
-      ground.rotation.x = Math.PI / 2;
-      ground.receiveShadows = true;
-      ground.checkCollisions = true;
+        return camera;
+      }
+      const camera = createCamera();
 
-      var wall1 = Mesh.CreateBox("w1", 3, scene);
-      wall1.checkCollisions = true;
-      wall1.position = new Vector3(8, 4.5, 5);
+      const createMap = function() {
+        var ground = Mesh.CreatePlane("g", 120, scene);
+        ground.position = new Vector3(0, 0, 0);
+        ground.rotation.x = Math.PI / 2;
+        ground.receiveShadows = true;
+        ground.checkCollisions = true;
 
-      var wall2 = Mesh.CreateBox("w2", 3, scene);
-      wall2.checkCollisions = true;
-      wall2.position = new Vector3(11, 1.5, 5);
+        var wall1 = Mesh.CreateBox("w1", 3, scene);
+        wall1.checkCollisions = true;
+        wall1.position = new Vector3(8, 4.5, 5);
 
-      var wall3 = Mesh.CreateBox("w3", 3, scene);
-      wall3.checkCollisions = true;
-      wall3.position = new Vector3(5, 1.5, 5);
+        var wall2 = Mesh.CreateBox("w2", 3, scene);
+        wall2.checkCollisions = true;
+        wall2.position = new Vector3(11, 1.5, 5);
 
-      
+        var wall3 = Mesh.CreateBox("w3", 3, scene);
+        wall3.checkCollisions = true;
+        wall3.position = new Vector3(5, 1.5, 5);
 
-      let tempv = new Vector3(0, 0, 0);
+        return ground;
+      }
+      const ground = createMap();
 
       scene.registerBeforeRender(function () {
           // Player speed
