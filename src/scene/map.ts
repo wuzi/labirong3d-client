56519c3ebb7cb2cc7edb58e599757f29bd0684b1
Lucas Godoy <lcsdegodoy@outlook.com>
2020-04-07 10:34:39 -0300

    feat: make only the ground pickable

--- src/scene/map.ts
+++ src/scene/map.ts
@@ -4,6 +4,11 @@ const loadMap = async (scene: Scene): Promise<void> => {
   const { meshes } = await SceneLoader.ImportMeshAsync('', 'assets/', 'village.obj', scene);
   meshes.map((m) => {
     const mesh = m;
+
+    if (mesh.name.search('terrain_grass') === -1) {
+      mesh.isPickable = false;
+    }
+
     mesh.checkCollisions = true;
     return mesh;
   });
