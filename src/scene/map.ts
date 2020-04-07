0eb5a50cf4a5bcfc95c9573557640d764c7e2ba3
Igor Souza <igor.souza.webmaster@gmail.com>
2020-04-06 22:04:18 -0300

    refactor: map import

--- src/scene/map.ts
+++ src/scene/map.ts
@@ -1,12 +1,12 @@
 import { SceneLoader, Scene } from '@babylonjs/core';
 
 const loadMap = (scene: Scene): void => {
-  SceneLoader.ImportMesh('', 'assets/', 'village.obj', scene, (objects) => {
-    objects.map((obj) => {
-      const object = obj;
-      object.checkCollisions = true;
+  SceneLoader.ImportMesh('', 'assets/', 'village.obj', scene, (meshes) => {
+    meshes.map((meshe) => {
+      const finalMeshe = meshe;
+      finalMeshe.checkCollisions = true;
 
-      return object;
+      return meshe;
     });
   });
 };
