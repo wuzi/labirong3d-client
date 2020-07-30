5a72a504ed7601d2aa664bfa32684875659e5f1a
Lucas Godoy <lcsdegodoy@outlook.com>
2020-07-30 15:50:17 -0300

    feat: create world using data from server

--- src/game/index.ts
+++ src/game/index.ts
@@ -14,7 +14,7 @@ export default class Game {
 
   public readonly players: Player[] = [];
 
-  public readonly grid: number[][] = [];
+  public grid: number[][] = [];
 
   constructor(public readonly network: Network) {
     this.canvas = document.getElementById('canvas') as HTMLCanvasElement;
@@ -25,14 +25,45 @@ export default class Game {
     this.ground.checkCollisions = true;
 
     this.network.connection.onopen = (): void => {
-      this.syncRemotePlayers();
+      this.network.send('syncWorld');
+    };
+
+    this.network.connection.onmessage = (e: MessageEvent): void => {
+      const event = JSON.parse(e.data);
+      if (event.name === 'syncWorld') {
+        event.data.players.forEach((remotePlayer: RemotePlayer) => {
+          this.addPlayer(remotePlayer);
+        });
+        this.grid = event.data.grid;
+        this.spawnWalls();
+      } else if (event.name === 'playerJoin') {
+        this.addPlayer(event.data.player);
+      } else if (event.name === 'playerQuit') {
+        this.removePlayer(event.data.id);
+      } else if (event.name === 'update') {
+        event.data.players.forEach((remotePlayer: RemotePlayer) => {
+          const player = this.players.find((pl) => pl.id === remotePlayer.id);
+          if (!player) return;
+
+          player.position.x = remotePlayer.position.x;
+          player.position.y = remotePlayer.position.y;
+          player.position.z = remotePlayer.position.z;
+
+          player.rotation.x = remotePlayer.rotation.x;
+          player.rotation.y = remotePlayer.rotation.y;
+          player.rotation.z = remotePlayer.rotation.z;
+        });
+      }
+    };
+
+    this.network.connection.onerror = (err): void => {
+      // eslint-disable-next-line no-console
+      console.log(err);
     };
 
     if (process.env.NODE_ENV === 'development') {
       this.scene.debugLayer.show();
     }
-
-    this.spawnWalls();
   }
 
   private async addPlayer(remotePlayer: RemotePlayer): Promise<void> {
@@ -58,51 +89,14 @@ export default class Game {
     player.mesh.dispose();
   }
 
-  private syncRemotePlayers(): Promise<void> {
-    return new Promise(((resolve, reject) => {
-      this.network.send('getConnectedPlayers');
-
-      this.network.connection.onmessage = (e: MessageEvent): void => {
-        const event = JSON.parse(e.data);
-        if (event.name === 'getConnectedPlayers') {
-          event.data.players.forEach((remotePlayer: RemotePlayer) => {
-            this.addPlayer(remotePlayer);
-          });
-          resolve(undefined);
-        } else if (event.name === 'playerJoin') {
-          this.addPlayer(event.data.player);
-        } else if (event.name === 'playerQuit') {
-          this.removePlayer(event.data.id);
-        } else if (event.name === 'update') {
-          event.data.players.forEach((remotePlayer: RemotePlayer) => {
-            const player = this.players.find((pl) => pl.id === remotePlayer.id);
-            if (!player) return;
-
-            player.position.x = remotePlayer.position.x;
-            player.position.y = remotePlayer.position.y;
-            player.position.z = remotePlayer.position.z;
-
-            player.rotation.x = remotePlayer.rotation.x;
-            player.rotation.y = remotePlayer.rotation.y;
-            player.rotation.z = remotePlayer.rotation.z;
-          });
-        }
-      };
-
-      this.network.connection.onerror = (err): void => {
-        reject(err);
-      };
-    }));
-  }
-
   private spawnWalls(): void {
-    for (let x = 0; x < this.grid.length; x++) {
-      for (let z = 0; z < this.grid[x].length; z++) {
-        if (this.grid[x][z] === 1) {
+    this.grid.forEach((tiles, x) => {
+      tiles.forEach((tile, z) => {
+        if (tile === 1) {
           const wall = new Wall(this.scene);
           wall.position = new BABYLON.Vector3((x * 8) - 64, 0, (z * 8) - 64);
         }
-      }
-    }
+      });
+    });
   }
 }
