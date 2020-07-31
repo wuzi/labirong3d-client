564bdb0ed6aefd87038a861c405204293006423b
Lucas Godoy <lcsdegodoy@outlook.com>
2020-07-31 20:48:58 -0300

    refactor: change camera lower radius limit

--- src/camera/follow.ts
+++ src/camera/follow.ts
@@ -10,20 +10,19 @@ export default class FollowCamera {
     private readonly game: Game,
     private readonly mesh: BABYLON.Mesh,
   ) {
-    this.camera = new BABYLON.FollowCamera('FollowCam', this.mesh.position.add(new BABYLON.Vector3(0.0, 5.0, -45.0)), this.game.scene);
-    this.setCameraLimits();
+    this.camera = new BABYLON.FollowCamera('FollowCam', this.mesh.position, this.game.scene);
+    this.camera.radius = FollowCamera.CAMERA_DISTANCE;
+
+    this.camera.lowerRadiusLimit = 2;
+    this.camera.upperRadiusLimit = FollowCamera.CAMERA_DISTANCE;
+
+    this.camera.lowerHeightOffsetLimit = 0.5;
+    this.camera.upperHeightOffsetLimit = FollowCamera.CAMERA_DISTANCE;
+
     this.camera.attachControl(this.game.canvas, true);
   }
 
   public lockTarget(target: BABYLON.Nullable<BABYLON.AbstractMesh>): void {
     this.camera.lockedTarget = target;
-  }
-
-  private setCameraLimits(): void {
-    this.camera.radius = FollowCamera.CAMERA_DISTANCE;
-    this.camera.lowerRadiusLimit = -FollowCamera.CAMERA_DISTANCE;
-    this.camera.upperRadiusLimit = FollowCamera.CAMERA_DISTANCE;
-    this.camera.lowerHeightOffsetLimit = 0.5;
-    this.camera.upperHeightOffsetLimit = FollowCamera.CAMERA_DISTANCE;
   }
 }
