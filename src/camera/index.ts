90410cc84ee48754b0ce11c8c1e716aa6baa03d6
Lucas Godoy <lcsdegodoy@outlook.com>
2020-08-01 00:11:25 -0300

    feat: limit camera upper beta

--- src/camera/index.ts
+++ src/camera/index.ts
@@ -12,8 +12,9 @@ export default class Camera {
     this.camera = new BABYLON.ArcRotateCamera('PlayerCam', 0, 0.8, Camera.CAMERA_DISTANCE, new BABYLON.Vector3(0, 0, 0), this.game.scene);
 
     this.camera.checkCollisions = true;
-    this.camera.collisionRadius = new BABYLON.Vector3(1, 2, 1);
+    this.camera.collisionRadius = new BABYLON.Vector3(1, 0.5, 1);
 
+    this.camera.upperBetaLimit = Math.PI / 2;
     this.camera.lowerRadiusLimit = 5;
     this.camera.upperRadiusLimit = Camera.CAMERA_DISTANCE;
 
