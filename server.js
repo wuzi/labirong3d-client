c798bff6eca2fbfd1a4896c108a19d890a685cec
Lucas Godoy <lcsdegodoy@outlook.com>
2023-02-04 10:49:48 -0300

    fix: get port from env

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static('dist'));
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
